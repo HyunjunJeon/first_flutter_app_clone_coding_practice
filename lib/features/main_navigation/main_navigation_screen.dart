b48ac2cba0513a79a63a4c418bbf67029fd29e36
HyunjunJeon <jeonhj920@gmail.com>
2023-02-11 17:16:43 +0900

    [Feature - MainNavigationScreen] 1. Main Navigation  - Offstage Widget: 위젯을 렌더링 하되, 보여주진 않음(자원 소모가 심할 수 있음)  - Custom Buttom(PostVideoButton): Stack 으로 쌓아올리고 Positioned Widget 으로 위치를 옮겨줘서 Buttom 을 만듦

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tiktok_clone/constant/gaps.dart';
+import 'package:flutter_tiktok_clone/constant/sizes.dart';
+import 'package:flutter_tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
+import 'package:flutter_tiktok_clone/features/onboarding/widgets/post_video_button.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainNavigationScreen extends StatefulWidget {
@@ -12,44 +16,113 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = const [
-    Center(
-      child: Text("Home"),
-    ),
-    Center(
-      child: Text("Search"),
-    ),
-    Center(
-      child: Text("Search"),
-    ),
-    Center(
-      child: Text("Search"),
-    ),
-    Center(
-      child: Text("Search"),
-    ),
-  ];
-
   void _onTap(int idx) {
     setState(() {
       _selectedIndex = idx;
     });
   }
 
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(title: const Text("Record Videos")),
+        ),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.house),
+    return Scaffold(
+      body: Stack(children: [
+        Offstage(
+          offstage: _selectedIndex != 0,
+          child: const Text(
+            "Home",
+            style: TextStyle(
+              fontSize: Sizes.size36,
+              color: Colors.black,
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+        ),
+        Offstage(
+          offstage: _selectedIndex != 1,
+          child: const Text(
+            "Discover",
+            style: TextStyle(
+              fontSize: Sizes.size36,
+              color: Colors.black,
+            ),
           ),
-        ],
+        ),
+        Offstage(
+          offstage: _selectedIndex != 3,
+          child: const Text(
+            "Inbox",
+            style: TextStyle(
+              fontSize: Sizes.size36,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Offstage(
+          offstage: _selectedIndex != 4,
+          child: const Text(
+            "Profile",
+            style: TextStyle(
+              fontSize: Sizes.size36,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ]),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: "Home",
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                selectedIcon: FontAwesomeIcons.compass,
+                onTap: () => _onTap(1),
+              ),
+              Gaps.h24,
+              GestureDetector(
+                onTap: _onPostVideoButtonTap,
+                child: const PostVideoButton(),
+              ),
+              Gaps.h24,
+              NavTab(
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
       ),
-      tabBuilder: (context, index) => screens[index],
     );
   }
 }
