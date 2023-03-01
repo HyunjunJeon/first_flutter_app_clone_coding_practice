a85bf729770cdad0a2c09045766eb44f5b8df1b6
HyunjunJeon <jeonhj920@gmail.com>
2023-03-01 13:12:16 +0900

    [Feature - Responsive flutter Web] 1. OrientationBuilder 를 활용해 가로, 세로 대응 2. SystemChrome 활용해서 가로, 세로 고정  - SystemChrome 을 활용해서 시스템 Status Bar 도 컨트롤 가능
    3. WidgetsFlutterBinding.ensureInitialized() 를 이용해 Flutter 엔진과 위젯의 바인딩이 미리 완료되어 있게 만들어줌 => main 쪽에서 Server 와 통신하거나, SharedPreferences 등 비동기로 데이터를 다룬 다음에 runApp 을 실행하여야 하는 경우

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -27,82 +27,117 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        // Status Bar 를 피해서 만들 수 있음
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: Sizes.size40,
-          ),
-          child: Column(
-            children: [
-              Gaps.v80,
-              const Text(
-                "Sign up for Tiktok",
-                style: TextStyle(
-                  fontSize: Sizes.size24,
-                  fontWeight: FontWeight.w700,
-                ),
+    return OrientationBuilder(
+      builder: (BuildContext context, Orientation orientation) {
+        // if (orientation == Orientation.landscape) {
+        //   return const Scaffold(
+        //     body: Center(child: Text("Please rotate your phone.")),
+        //   );
+        // }
+        return Scaffold(
+          body: SafeArea(
+            // Status Bar 를 피해서 만들 수 있음
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size40,
               ),
-              Gaps.v20,
-              const Text(
-                "Create a profile, follow other accounts, make your own videos, and more.",
-                style: TextStyle(
-                  fontSize: Sizes.size16,
-                  color: Colors.black54,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              Gaps.v40,
-              GestureDetector(
-                onTap: () => _onEmailTap(context),
-                child: const AuthButton(
-                  text: "Use phone or email",
-                  icon: FaIcon(FontAwesomeIcons.user),
-                ),
-              ),
-              Gaps.v16,
-              const AuthButton(
-                text: "Continue with Apple",
-                icon: FaIcon(FontAwesomeIcons.apple),
-              ),
-              Gaps.v16,
-              const AuthButton(
-                text: "Continue with Google",
-                icon: FaIcon(FontAwesomeIcons.google),
-              ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.grey.shade50,
-        elevation: Sizes.size1,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.size32,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Already have an account?",
-              ),
-              Gaps.h5,
-              GestureDetector(
-                onTap: () => _onLoginTap(context),
-                child: Text(
-                  " Log in",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontWeight: FontWeight.w600,
+              child: Column(
+                children: [
+                  Gaps.v80,
+                  const Text(
+                    "Sign up for Tiktok",
+                    style: TextStyle(
+                      fontSize: Sizes.size24,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
-                ),
+                  Gaps.v20,
+                  const Text(
+                    "Create a profile, follow other accounts, make your own videos, and more.",
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      color: Colors.black54,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Gaps.v40,
+                  // Collection IF 에서 여러개를 다루는 방법: List 에 넣고 ...[] 를 통해 리스트를 풀어서 반환하도록
+                  if (orientation == Orientation.portrait) ...[
+                    GestureDetector(
+                      onTap: () => _onEmailTap(context),
+                      child: const AuthButton(
+                        text: "Use phone or email",
+                        icon: FaIcon(FontAwesomeIcons.user),
+                      ),
+                    ),
+                    Gaps.v16,
+                    const AuthButton(
+                      text: "Continue with Apple",
+                      icon: FaIcon(FontAwesomeIcons.apple),
+                    ),
+                    Gaps.v16,
+                    const AuthButton(
+                      text: "Continue with Google",
+                      icon: FaIcon(FontAwesomeIcons.google),
+                    )
+                  ],
+                  // 가로모드 일 때
+                  if (orientation == Orientation.landscape) ...[
+                    Row(
+                      children: [
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () => _onEmailTap(context),
+                            child: const AuthButton(
+                              text: "Use phone or email",
+                              icon: FaIcon(FontAwesomeIcons.user),
+                            ),
+                          ),
+                        ),
+                        Gaps.h16,
+                        const Expanded(
+                          child: AuthButton(
+                            text: "Continue with Apple",
+                            icon: FaIcon(FontAwesomeIcons.apple),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+          bottomNavigationBar: BottomAppBar(
+            color: Colors.grey.shade50,
+            elevation: Sizes.size1,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size32,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already have an account?",
+                  ),
+                  Gaps.h5,
+                  GestureDetector(
+                    onTap: () => _onLoginTap(context),
+                    child: Text(
+                      " Log in",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_tiktok_clone/constant/sizes.dart';
-import 'package:flutter_tiktok_clone/features/main_navigation/main_navigation_screen.dart';
+import 'package:flutter_tiktok_clone/features/authentication/sign_up_screen.dart';
 
-void main() {
+void main() async {
+  // 꼭 main 의 runApp 이전에 쓰여야함
+  WidgetsFlutterBinding.ensureInitialized(); // Flutter Engine 과 Widget 들을 결합시켜줌
+
+  await SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp, // 항상 세로 Up 모드로 고정되게끔(유저가 화면을 돌려도)
+    ],
+  );
+
+  // 시스템 status bar(시계, 배터리 등 나오는 최상단 부분) - 꼭 여기서 쓸 필욘 없고 모든 화면에서 컨트롤 가능
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.dark,
+  );
+
   runApp(const TikTokCloneApp());
 }
 
@@ -12,6 +27,8 @@ class TikTokCloneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // iOS Emulator 에서 debug 글자 안보이게
+      debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       theme: ThemeData(
         // 최대한 공통이 되는 Theme Data 는 모아두기(Material Design 에서만 사용 가능)
@@ -35,7 +52,7 @@ class TikTokCloneApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const MainNavigationScreen(),
+      home: const SignUpScreen(),
     );
   }
 }
