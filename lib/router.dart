c0506edc66c825ff59be9b6d44c940cbfa66aeeb
HyunjunJeon <jeonhj920@gmail.com>
2023-03-12 22:00:11 +0900

    [Feature - Video Recording] 1. Video Recoding 에 필요한 pubspec - camera: 0.10.3+2 라이브러리 설치 2. 실제 iPhone 연결을 위한 ios/Runner/info.plist 변경  - Debug 모드 시 오류 해결을 위한 https://docs.flutter.dev/development/add-to-app/ios/project-setup#apple-silicon-arm64-macs

--- lib/router.dart
+++ lib/router.dart
@@ -1,66 +1,9 @@
-import 'package:flutter_tiktok_clone/features/authentication/email_screen.dart';
-import 'package:flutter_tiktok_clone/features/authentication/sign_up_screen.dart';
-import 'package:flutter_tiktok_clone/features/authentication/username_screen.dart';
-import 'package:flutter_tiktok_clone/features/users/user_profile_screen.dart';
+import 'package:flutter_tiktok_clone/features/videos/video_recording_screen.dart';
 import 'package:go_router/go_router.dart';
-
-import 'features/authentication/login_screen.dart';
 
 final router = GoRouter(routes: [
   GoRoute(
-    name: SignUpScreen.routeName,
-    path: SignUpScreen.routeURL,
-    builder: (context, state) => const SignUpScreen(),
-    routes: [
-      // route 를 nested 하게 작성해서 ex> /users/:id/comments/:commect_num/ ... 등으로 만들 수 있음
-      GoRoute(
-        name: UsernameScreen.routeName,
-        path: UsernameScreen.routeURL,
-        builder: (context, state) => const UsernameScreen(),
-        routes: [
-          GoRoute(
-            name: EmailScreen.routeName,
-            path: EmailScreen.routeURL,
-            builder: (context, state) {
-              final args = state.extra as EmailScreenArgs;
-              return EmailScreen(username: args.username);
-            },
-          ),
-        ],
-      ),
-    ],
-  ),
-  GoRoute(
-    path: LoginScreen.routeName,
-    builder: (context, state) => const LoginScreen(),
-  ),
-  // GoRoute(
-  //   name: "username_screen",
-  //   path: UsernameScreen.routeName,
-  //   pageBuilder: (context, state) {
-  //     // page Builder 를 사용해서 페이지가 나올 때 Animation 적용이 가능하다
-  //     return CustomTransitionPage(
-  //         child: const UsernameScreen(),
-  //         transitionDuration: const Duration(seconds: 2),
-  //         transitionsBuilder: (context, animation, secondaryAnimation, child) {
-  //           return FadeTransition(
-  //             opacity: animation,
-  //             child: ScaleTransition(
-  //               scale: animation,
-  //               child: child,
-  //             ),
-  //           );
-  //         });
-  //   },
-  // ),
-  GoRoute(
-    path: "/users/:username",
-    builder: (context, state) {
-      final username = state.params['username'];
-      // ??= null-aware assignment operator: 변수가 null 이면 값을 할당하고, 아니면 값을 유지하는 연산자
-      String? tab = state.queryParams["show"];
-      tab ??= "posts";
-      return UserProfileScreen(username: username!, tab: tab);
-    },
-  ),
+    path: "/",
+    builder: (context, state) => const VideoRecodingScreen(),
+  )
 ]);
