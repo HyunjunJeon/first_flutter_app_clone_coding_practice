f9e8883d5edc6c2280474b0c4053595f0ae1e564
HyunjunJeon <jeonhj920@gmail.com>
2023-03-14 14:12:41 +0900

    [Feature - Video Recording] 1. Permission Handling 에 필요한 Lib 설치 2. Video 쪽 통제할 화면 껍데기 만들어줌

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class VideoRecodingScreen extends StatelessWidget {
+class VideoRecodingScreen extends StatefulWidget {
   const VideoRecodingScreen({Key? key}) : super(key: key);
 
+  @override
+  State<VideoRecodingScreen> createState() => _VideoRecodingScreenState();
+}
+
+class _VideoRecodingScreenState extends State<VideoRecodingScreen> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
